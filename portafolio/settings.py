1fa15c35099f0d9de6cc7271cd22e3c21eae32a5
vmarialuzm <vmarialuzm@gmail.com>
2023-06-20 22:38:14 -0500

    config static

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -50,13 +50,14 @@ CSRF_TRUSTED_ORIGINS = ['https://proyectofinaldjangorailway-production.up.railwa
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'autenticacion.middleware.is_valid_ip'
+    'autenticacion.middleware.is_valid_ip',
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
@@ -131,7 +132,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'portafolio/static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
